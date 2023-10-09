95b0da4ddde140d8c749d46a96d49c21f4b30fab
Chansoo <devhan1207@gmail.com>
2023-10-08 23:32:23 -0400

    added js function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
 // scroll sections active link
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -16,7 +25,12 @@ window.onscroll = () => {
       });
     }
   });
+
   // sticky navbar
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 100);
+
+  // remove toggle icon and navbar when click navbar link (scroll)
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
